a53e03d64138f9aebcba7f329a07b9874914358f
chison2707 <chiphansonzz17@gmail.com>
2025-08-01 10:48:54 +0700

    seed prisma

---
+++ prisma/seeds/database.seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+const prisma = new PrismaClient();
+async function main() {
+  const saltOrRounds = 10;
+  const user = await prisma.user.create({
+    data: {
+      email: 'chiphansonzz17@gmail.conm',
+      password: await bcrypt.hash('password', saltOrRounds),
+      name: 'Chi Son',
+    },
+  });
+  console.log({ user });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
